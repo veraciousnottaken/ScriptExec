4401ab6be702a21ee1835dd41905e70b16a340e5
Hello World <veracious@wp.pl>
2013-05-05 00:39:33 +0200

    Changed namespace

--- Class/IniReader/IniFileReader.cs
+++ Class/IniReader/IniFileReader.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using System.Data;
 using System.IO;
+using IniReader;
 
-namespace IniReader
+namespace ScriptExec.Class.IniReader
 {
     public class IniFileReader
     {
